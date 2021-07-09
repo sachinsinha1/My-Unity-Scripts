2f799a51e3eb197ad4c9a2e8ff34771c02a6e2ce
sachinsinha1 <sachinsnh00@gmail.com>
2021-07-09 16:53:14 +0530

    Update door.cs

--- door.cs
+++ door.cs
@@ -49,8 +49,7 @@ public class door : MonoBehaviour
                     }
 
 
-                }
-                
+                }                
             }
         }
         if (doorOpen)
